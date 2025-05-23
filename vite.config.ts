733975eeef83b88866bde0cd401ce8e7e01382f2
Julie <ulabalaeva07768@gmail.com>
2025-05-23 12:18:01 +0300

    fix(config): change defineConfig, add deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: command === 'build' ? '/JulyBO7/' : '/',
     plugins: [react()],
     server: {
         host: true,
